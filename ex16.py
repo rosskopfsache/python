ff8f9beb9316a6fde4c1c19fa611a868fcea7207
Rosskopf, Lukas (I/FP-87) <lukas.rosskopf@audi.de>
2018-09-25 15:35:05 +0200

    continued

--- ex16.py
+++ ex16.py
@@ -1,10 +1,31 @@
 #!/usr/bin/python3.6
 
 import random
+import string
+import sys 
 
-def generatePassword():
-    numbers = random.sample(range(1,11),10)
-    chars = random.choice('a-Z')
-    return chars
+def generatePassword(numbers=True, letters=True, special=True, length=16):
+    '''
+    Randomly generating a password with following input parameters:
+        - boolean numbers [default:True]  >  include numbers in password
+        - boolean letters [default:True]  >  include letters in password
+        - boolean special [default:True]  >  include special characters in password
+        - int length [default:16]         >  length of generated password
+    '''
+    raw = []
+    if numbers is True:
+        numbers = list(range(length))
+        raw = raw + numbers
+    if letters is True:
+        letters = list(string.ascii_letters)
+        raw = raw + letters
+    if special is True:
+        special = ['!', '#', '+', ',','.','$','&','(',')','?','*']
+        raw = raw + special
+    #raw = numbers + letters + special
+    random.shuffle(raw)
+    pw = ''.join(str(i) for i in raw[:length])
+    
+    return pw
 
-print(generatePassword())
+print(generatePassword(True, True, True, 16))
---
+++ test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.6
+
+import random
+import random
+print(
+    
+    random.sample(range(1000), 10)
+
+
+)
